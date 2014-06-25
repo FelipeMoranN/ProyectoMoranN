bb18840d7948a3ec890d2d0c58f37eef6cdcb77b
alum.fial7 <alum.fial7@FIA-LAB7C5.lim.upeu.edu.pe>
2014-06-25 15:27:41 -0500

    Version4 :listados

--- src/pe/upeu/edu/moran/DAO/DistritoDAO.java
+++ src/pe/upeu/edu/moran/DAO/DistritoDAO.java
@@ -6,10 +6,55 @@
 
 package pe.upeu.edu.moran.DAO;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.upeu.edu.moran.config.Conexion;
+import pe.upeu.edu.moran.modelo.Distrito;
+
 /**
  *
  * @author alum.fial7
  */
 public class DistritoDAO {
+    private Connection cx;
+    private Statement st;
+    private ResultSet rs;
+    private String sql;
+    ArrayList<Distrito> lista = null;
+    public ArrayList<Distrito> listarDistrito(){
+        
+        lista = new ArrayList();
+        try {
+         sql="SELECT *FROM distrito";
+        cx=Conexion.getConex();
+        st=cx.createStatement();
+        rs=st.executeQuery(sql);
+        while(rs.next()){
+            Distrito d= new Distrito();
+            d.setIdd(rs.getInt("iddistrito"));
+            d.setIdr(rs.getInt("idregion"));
+            d.setDisc(rs.getString("distrito"));
+            d.setEstado(rs.getString("estado"));
+            lista.add(d);
+            
+            
+        }
+            
+        } catch (SQLException e) {
+        }
+        
+        
+        
+        return lista;
+        
+    }
+    
+    
+    
+    
     
 }
--- src/pe/upeu/edu/moran/DAO/TipoIglesiaDAO.java
+++ src/pe/upeu/edu/moran/DAO/TipoIglesiaDAO.java
@@ -6,10 +6,48 @@
 
 package pe.upeu.edu.moran.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.upeu.edu.moran.config.Conexion;
+import pe.upeu.edu.moran.modelo.Distrito;
+import pe.upeu.edu.moran.modelo.TipoIglesia;
+
 /**
  *
  * @author alum.fial7
  */
 public class TipoIglesiaDAO {
-    
+    private Connection cx;
+    private Statement st;
+    private ResultSet rs;
+    private String sql;
+    ArrayList<TipoIglesia> lista = null;
+    public ArrayList<TipoIglesia> listarTipoIglesia(){
+        
+        lista = new ArrayList();
+        try {
+         sql="SELECT *FROM tipo_iglesia";
+        cx=Conexion.getConex();
+        st=cx.createStatement();
+        rs=st.executeQuery(sql);
+        while(rs.next()){
+            TipoIglesia ti= new TipoIglesia();
+            ti.setIdti(rs.getInt("iddistrito"));
+            ti.setNomtipo(rs.getString("idregion"));
+            
+            lista.add(ti);
+            
+            
+        }
+            
+        } catch (SQLException e) {
+        }
+        
+        
+        
+        return lista;
+    }
 }
